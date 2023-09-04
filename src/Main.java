e3fafa6c99a14941f21c952bd698d00b55827cc3
saman <zobeirysaman@gmail.com>
2023-09-04 19:37:03 +0100

    changed the printer pages

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Printer printer = new Printer(50, false);
         System.out.println("initial page count = " + printer.getPagesPrinted());
 
-        int pagesPrinted = printer.printPages(5);
+        int pagesPrinted = printer.printPages(10);
         System.out.printf("Current Job Pages: %d, Printer Total: %d %n",
                 pagesPrinted, printer.getPagesPrinted());
 
